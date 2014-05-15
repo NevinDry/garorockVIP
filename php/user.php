f0dba5a7d9454640e0152f4d704192fb460cb4e4
Nevindry <puissant.etienne@gmail.com>
2014-05-15 14:31:52 +0200

    Integration fichier json

--- php/user.php
+++ php/user.php
@@ -1,5 +1,4 @@
 <?php 
-
 if(isset($_GET["mail"]))
 {
    $mail = $_GET["mail"];
@@ -16,4 +15,13 @@ if(isset($_GET["team"]))
 }
 
 print_r($team." ".$mail." ".$nom);
+
+$fichierUser = '../users.json';
+
+if(!$fp = fopen($fichierUser, 'w+')){
+	echo "Echec de l'ouverture du fichier";
+	exit;
+} else {
+	echo "good";
+}
 ?>
